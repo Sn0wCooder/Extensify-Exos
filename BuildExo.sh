ab9a2b09d0be8ebdf6483c06e155ac3923f9a00a
Sn0wCooder <leoalfreducci@gmail.com>
2020-06-26 18:52:10 +0200

    Updated BuildExo.sh to version 1.1

--- BuildExo.sh
+++ BuildExo.sh
@@ -1,139 +1,133 @@
 #!/bin/bash
 
+# Script version: 1.1
+
+# TODO first of all: check if brew and all tools required are installed
+
+# Check if optool is installed
+
+OUTPUT=~/Desktop
+
 if which optool >/dev/null; then
-    echo "optool is installed"
+    #echo "optool is installed"
+    echo
 else
 	echo "optool is NOT installed. Proceed with installation..."
-    cd /usr/local/bin > /dev/null 2>&1
+  cd /usr/local/bin > /dev/null 2>&1
 	sudo curl -OL https://github.com/alexzielenski/optool/releases/download/0.1/optool.zip > /dev/null 2>&1
 	sudo unzip /usr/local/bin/optool.zip -d /usr/local/bin > /dev/null 2>&1
 	sudo rm -rf optool.zip > /dev/null 2>&1
-	cd
 fi
 
-echo "Insert here the cracked IPA:"
+# List all available apps:
+echo "Only the following apps are *at the moment* supported:"
+echo
+echo
+svn ls https://github.com/Sn0wCooder/Extensify-Exos/trunk | grep "/" | rev | cut -c2- | rev
+echo
+echo
+
+# Initialization
+
+echo "End directory: $OUTPUT"
+
+TMPDIR=$(mktemp -d -t ci-XXXXXXXXXX)
+cd $TMPDIR
+#open $TMPDIR
+
+echo "Insert here the decrypted IPA:"
 read crackedipa
 
-echo "End directory: ~/Desktop"
-
-rm -rf tempdire > /dev/null 2>&1
-mkdir tempdire
-cd tempdire
 unzip `realpath "$crackedipa"` > /dev/null 2>&1
 
 filename=$(basename Payload/*)
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-echo "IPA selected for build Exo: $filename"
+echo
+echo
+echo "Application selected: $filename"
+echo
+echo
 
-echo "Downloading Exo(s)... (it will take a while!)"
+# List exo(s)
 
-#tweakv=$(svn ls https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename) > /dev/null 2>&1
+echo "Choose the exo(s) you want:"
+i=1
+array=($(svn ls https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename | grep "/" | rev | cut -c2- | rev))
+for (( length=1; length <= "${#array[@]}"; ++length )); do
+    for (( start=0; start + length <= "${#array[@]}"; ++start )); do
+        EXOS_AVAIL=$(echo "${array[@]:start:length}")
+        echo "$i) $EXOS_AVAIL"
+        echo $EXOS_AVAIL | tr " " "\n" >> $i
+        ((i=i+1))
+    done
+done
+last=$(( i - 1 ))
+echo "$i) Exit"
 
-#echo $tweakv >> tweakv.txt
-#
-#
-#
-svn ls https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename >> ../tweakv.txt
-EXT=dylib
-cat ../tweakv.txt | while read p
+# Now you have to choose the exo(s) you want:
+
+OK=0
+while [ $OK != 1 ]
 do
-
-	rm -rf *
-
-	svn checkout https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename/$p > /dev/null 2>&1
-
-	ditto -xk $crackedipa . > /dev/null 2>&1
-
-	p=${p%/}
-
-	echo $p >> ../twk.txt
-
-	echo "I'm building Exo $p..."
-
-	cp -r $p/* Payload/$filename.app/
-
-	for i in $p/*; do
-		if [ "${i}" != "${i%.${EXT}}" ];then
-			dylibname=$(basename $i)
-			echo "Injecting $dylibname..."
-			optool install -c load -p @executable_path/$dylibname -t Payload/*/$filename > /dev/null 2>&1
-		fi
-	done
-
-	echo "Repacking..."
-	zip -9qry "app.ipa" "Payload" > /dev/null 2>&1
-	BUNDLE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Payload/*/Info.plist)
-
-	newfilename=$filename-v$BUNDLE_VERSION-$p.ipa
-	newfilename=`echo $newfilename|tr '-' '_'`
-
-	mv app.ipa ~/Desktop/$newfilename
-
-	#rm -rf app.ipa
-
-	echo "Saved with filename $newfilename"
-
+  read opt
+  case $opt in
+    [1-$last] )
+      OK=1
+      echo "Good choice!"
+      ;;
+    $i )
+      OK=1
+      echo "Goodbye!"
+      break
+      ;;
+    *)
+      OK=0
+      echo "Wrong choice. Retry."
+      ;;
+    esac
 done
 
+# Download exo(s) the user chose:
 
+mkdir ToInject
+cd ToInject
 
-# if number of ipas > 1
-echo
-echo
-echo
+while read p; do
+  echo "Downloading $p..."
+  svn checkout https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename/$p > /dev/null 2>&1
+  mv $p/* .
+  rm -rf $p
+done < ../$opt
 
-rm -rf *
-cat ../tweakv.txt | while read p
-do
-	svn checkout https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename/$p > /dev/null 2>&1
+# Inject dylib(s)
+
+for DYLIB in ./*.dylib; do
+  DYLIBNAME=$(basename $DYLIB)
+  echo "Injecting $DYLIBNAME..."
+  optool install -c load -p @executable_path/$dylibname -t ../Payload/*/$filename > /dev/null 2>&1
 done
 
-NUMBEROFTWEAKS=$(find ./* -maxdepth 0 -type d | wc -l)
+# Move files
 
-echo "Number of tweaks: $NUMBEROFTWEAKS"
+cd ..
+mv ToInject/* Payload/*/
 
-if (( NUMBEROFTWEAKS > 1 )); then
+# Repack
 
+echo "Repacking..."
 
+exos=$(cat ./$opt)
+exos=$(echo "${exos//$'\n'/_}")
+zip -9qry "app.ipa" "Payload" > /dev/null 2>&1
+BUNDLEVERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Payload/*/Info.plist)
 
-	ditto -xk $crackedipa . > /dev/null 2>&1
+newfilename=$filename-v$BUNDLEVERSION-$exos.ipa
+newfilename=`echo $newfilename|tr '-' '_'`
+mv app.ipa $OUTPUT/$newfilename
 
-	cat ../twk.txt | while read p
-	do
-		echo -n "_$p" >> ../tweakadded.txt
-		cp -r $p/* Payload/$filename.app
+# Clean up
+rm -rf $TMPDIR
 
-		for i in $p/*; do
-			if [ "${i}" != "${i%.${EXT}}" ];then
-				dylibname=$(basename $i)
-				echo "Injecting $dylibname..."
-				optool install -c load -p @executable_path/$dylibname -t Payload/*/$filename > /dev/null 2>&1
-			fi
-		done
-	done
-
-	echo "Repacking..."
-	zip -9qry "app.ipa" "Payload" > /dev/null 2>&1
-	BUNDLE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Payload/*/Info.plist)
-
-#cat ../twk.txt | while read p
-#do
-#	echo "_$p" > ../tweakadded.txt
-#done
-
-	tweakadded=$(cat ../tweakadded.txt)
-
-	newfilename=$filename-v$BUNDLE_VERSION$tweakadded.ipa
-	newfilename=`echo $newfilename|tr '-' '_'`
-
-	mv app.ipa ~/Desktop/$newfilename
-
-fi
-
-rm -rf ../tweakv.txt > /dev/null 2>&1
-rm -rf ../twk.txt > /dev/null 2>&1
-rm -rf ../tweakadded.txt > /dev/null 2>&1
-
-echo "FINISHED!"
+echo "Finished!"
